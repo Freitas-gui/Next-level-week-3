b30bcdd8e4a597c8c14e2d570db597da088d6e74
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-10-14 17:33:11 -0300

    finished backend

--- lessons/backend/src/controllers/OrphanagesController.ts
+++ lessons/backend/src/controllers/OrphanagesController.ts
@@ -2,78 +2,88 @@ import { Request, Response } from 'express'
 
 import { getRepository } from 'typeorm'
 import Orphanage from '../models/Orphanage'
+import orphanageView from '../views/orphanages_view'
+
+import * as Yup from 'yup'
 
 export default {
   async index(request: Request, response: Response){
-    try {  
-      const orphanagesRepository = getRepository(Orphanage)
+    const orphanagesRepository = getRepository(Orphanage)
 
-      const orphanages = await orphanagesRepository.find({
-        relations:['images']
-      })
+    const orphanages = await orphanagesRepository.find({
+      relations:['images']
+    })
 
-      return response.json(orphanages)
-    } catch(err){
-      console.log(err)
-    }
+    return response.json(orphanageView.renderMany(orphanages))
   },
 
   async show(request: Request, response: Response){
-    try {
-      const { id } = request.params
+    const { id } = request.params
 
-      const orphanagesRepository = getRepository(Orphanage)
+    const orphanagesRepository = getRepository(Orphanage)
 
-      const orphanage = await orphanagesRepository.findOneOrFail(id, {
-        relations:['images']
-      })
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations:['images']
+    })
 
-      return response.json(orphanage)
-    } catch(err){
-        console.log(err)
-      }
+    return response.json(orphanageView.render(orphanage))
   },
   
   async create(request: Request, response: Response){
-
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      } = request.body
     
-    try {
-      const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-        } = request.body
-      
-      
-      const orphanagesRepository = getRepository(Orphanage)
-      
-      const requestImages = request.files as Express.Multer.File[]
-      
-      const images = requestImages.map(image => {
-        return { path: image.filename }
-      })
+    
+    const orphanagesRepository = getRepository(Orphanage)
+    
+    const requestImages = request.files as Express.Multer.File[]
+    
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
 
-      const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-        images
-      })
-      
-      await orphanagesRepository.save(orphanage)
-      
-      return response.status(201).json(orphanage)
-    } catch(err){
-      console.log(err)
-    } 
+    const data = {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      images
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required(),
+        })
+      )
+    })
+
+    await schema.validate(data, {
+      abortEarly: false,
+    })
+
+    const orphanage = orphanagesRepository.create(data)
+    
+    await orphanagesRepository.save(orphanage)
+    
+    return response.status(201).json(orphanage) 
     
   }
 
--- lessons/backend/src/server.ts
+++ lessons/backend/src/server.ts
@@ -1,14 +1,22 @@
 import express from 'express'
+
 import './database/connection'
+import path from 'path'
+import cors from 'cors'
+import 'express-async-errors'
 
 import routes from './routes'
+import errorHandle from './errors/handler'
 
 
 /* Treatment of request and response */
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app. use(routes)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandle)
 
 app.listen(3333)
 
